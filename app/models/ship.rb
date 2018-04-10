f1623bdd6e59510fd8966529042bc49b10814bdb
Nadia Lee <miss.nadia.lee@gmail.com>
2018-04-10 20:52:38 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,16 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  PIRATES = []
+
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
 end
