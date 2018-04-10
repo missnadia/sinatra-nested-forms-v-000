6233be5fe7d727b278f1d038d13358952010b3be
Nadia Lee <miss.nadia.lee@gmail.com>
2018-04-10 20:56:10 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,16 +1,20 @@
 class Ship
   attr_accessor :name, :type, :booty
 
-  SHIPS = []
+  @@SHIPS = []
 
   def initialize(params)
     @name = params[:name]
     @type = params[:type]
     @booty = params[:booty]
-    SHIPS << self
+    @@SHIPS << self
   end
 
   def self.all
-    SHIPS
+    @@SHIPS
+  end
+
+  def self.clear
+    @@SHIPS.clear
   end
 end
