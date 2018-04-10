5fe532f8c73e28aaf846f48070fb3af1129ceb36
Nadia Lee <miss.nadia.lee@gmail.com>
2018-04-10 20:50:51 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,10 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @@all << self
+  end
 end
